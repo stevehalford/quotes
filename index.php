ab8ee382185f32532bf7e54c5d3090fa6d7496a2
Steve Halford <steve.halford@boxuk.com>
2014-01-12 14:09:34 +0000

    Add slim-json-api

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
 require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
+$app->view(new \JsonApiView());
+$app->add(new \JsonApiMiddleware());
 
-$app->get('/', function () {
-    echo "Hello";
+$app->get('/', function () use ($app) {
+    $app->render(200, array(
+        'msg' => 'welcome to my API!',
+    ));
 });
 
 $app->run();
